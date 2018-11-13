8c96144a56c385931faa90cc55e4267065e6ae26
aaa2015 <ayuan1129@qq.com>
2018-11-13 14:20:03 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ package com.jd.scan.test
 public class Hello {
 
   public static void main(String[] args) {
-    String str = "passport.jd.com";
+    String str = "test.passport.jd.com";
     System.out.println(str);
   }
 }
